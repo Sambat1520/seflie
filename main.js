65d14be702cae78afd62071da8785d0784d44de7
Sambat1520 <128104026+Sambat1520@users.noreply.github.com>
2023-09-07 18:25:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+var SpeechRecognition = window.webkitSpeechRecognition;//activa el rec de voz
+  
+var recognition = new SpeechRecognition(); // palabra que reconoce
+
+var Textbox = document.getElementById("textbox"); //salida del texto escuchado
+
+function start()
+{
+    Textbox.innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    Textbox.innerHTML = Content;
+    console.log(Content);
+      if(Content =="SIUU")
+      {
+       
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Tomando tu sefie...";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_selfie(); 
+        save();
+    }, 3000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
+
